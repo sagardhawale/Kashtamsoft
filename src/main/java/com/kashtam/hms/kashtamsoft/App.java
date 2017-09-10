97de06831f9b930b8acd3e3ebd11601d64d08999
sagardhawale <sagardhawale@kashtamtech.com>
2017-09-10 14:36:13 +0530

    First commit for project

---
+++ src/main/java/com/kashtam/hms/kashtamsoft/App.java
@@ -0,0 +1,14 @@
+package com.kashtam.hms.kashtamsoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hllo World2!" );
+    }
+}
